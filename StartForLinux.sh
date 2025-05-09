67d5cee9dd9c807c7190ecfd952a36606c382b33
Vchi <vchicompany@gmail.com>
2025-05-09 18:41:05 -0500

    StartForLinux.sh
    This is the Bash executable for Linux systems.

---
+++ StartForLinux.sh
@@ -0,0 +1,4 @@
+# When installing, move this to your Linux files. Then, you don't have to change your directory.
+chmod +x RadiantX
+./RadiantX.cpp
+# Or you could do: g++ RadiantX.cpp or ./RadiantX
